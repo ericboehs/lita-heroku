c71b9211f3cd096e998b1b0a8fbce49f4f9db15f
Eric Boehs <ericboehs@gmail.com>
2016-03-10 16:13:55 -0600

    Use oauth token for bearer; Dynamically get app_id

--- README.md
+++ README.md
@@ -10,6 +10,20 @@ Add lita-heroku to your Lita instance's Gemfile:
 gem "lita-heroku"
 ```
 
+Create an oauth token and set it in config:
+
+``` bash
+heroku plugins:install https://github.com/heroku/heroku-oauth
+heroku authorizations:create -d "Lita Bot"
+echo "HEROKU_OAUTH_TOKEN=YOUR_TOKEN" >> .env
+```
+
+In your lita config:
+
+``` ruby
+config.handlers.heroku.oauth_token = ENV.fetch("HEROKU_OAUTH_TOKEN")
+```
+
 ## Configuration
 
 TODO: Describe any configuration attributes the plugin exposes.
--- lib/lita/handlers/heroku.rb
+++ lib/lita/handlers/heroku.rb
@@ -2,8 +2,8 @@ require "json"
 module Lita
   module Handlers
     class Heroku < Handler
-      config :bearer, required: true
-      config :application_id, required: true
+      config :oauth_token, required: true
+      config :app_prefix
 
       route(/^hk\s+([^ ]+)\s+(.+)/, :heroku_cmd, command: true, help: {
         "hk [app name] [command]" => "example: 'lita hk production ps'"
@@ -15,7 +15,7 @@ module Lita
         if command == "deploy"
           heroku_deploy response
         else
-          response.reply `#{heroku_bin} #{command} -a hats-#{environment}`
+          response.reply `#{heroku_bin} #{command} -a #{config.app_prefix}#{environment}`
         end
       end
 
@@ -32,23 +32,33 @@ module Lita
       end
 
       route(/^hk deploy\s+(.+)/, :heroku_deploy, command: true, help: {
-        "hk deploy [environment]" => "example: 'lita hk production deploy'"
+        "hk [environment] deploy" => "example: 'lita hk production deploy'"
       })
 
       def heroku_deploy(response)
-        app_id = config.application_id
-        bearer = config.bearer
-        branch = "master"
+        bearer = config.oauth_token
+        app_name = "#{config.app_prefix}#{response.matches[0][0]}"
+        branch = response.matches[0][1] || "master"
+
+        apps = JSON.parse `curl -s "https://api.heroku.com/apps" -H "Authorization: Bearer #{bearer}" -H 'Accept: application/vnd.heroku+json; version=3'`
+        app = apps.select{|app| app["name"] == app_name }.first
+        app_id = app["id"]
 
         build_response = `curl -s "https://kolkrabbi.herokuapp.com/apps/#{app_id}/github/push" -H "Authorization: Bearer #{bearer}" -d '{"branch":"#{branch}"}'`
         build_response = JSON.parse build_response
 
         if build_response.key?("build") && build_response["build"]["status"] == "pending"
-          response.reply("Started deploy.")
+          response.reply("Deploying #{branch} to #{app_name}.")
         else
           $stdout.puts build_response
-          response.reply("Deploy could not be started. Check my logs for more information.")
+          response.reply("Deploy could not be started. Response: #{build_response}")
         end
+      end
+
+      def api(bearer, uri, data=nil)
+        cmd  = %Q{curl -s "https://kolkrabbi.herokuapp.com#{uri}" -H "Authorization: Bearer #{bearer}" -H 'range: name ..; order=asc, max=1000'}
+        cmd += %Q{-d '#{data}'} if data
+        JSON.parse `#{cmd}`
       end
 
       Lita.register_handler(self)
